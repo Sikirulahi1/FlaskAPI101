3d47663ddac17bbc08382b1cfd904b8251cb94cd
Sikirulahi <kareemsikiru2018@gmail.com>
2024-02-01 03:56:59 +0100

    Some throubleshooting today

--- redirect_and_abort.py
+++ redirect_and_abort.py
@@ -1,4 +1,4 @@
-from flask import Flask, abort, redirect, url_for, render_template
+from flask import Flask, abort, redirect, url_for, render_template, make_response
 
 app = Flask(__name__)
 
@@ -11,6 +11,12 @@ def index():
 #     abort(401)
 #     this_never_executed()
 
+# @app.errorhandler(404)
+# def page_not_found(error):
+#     return render_template('page_not_found.html'), 404
+
 @app.errorhandler(404)
-def page_not_found(error):
-    return render_template('page_not_found.html'), 404
+def page_not_found():
+    resp = make_response(render_template('error.html'), 404)
+    resp.headers['Error404'] = 'Error in this page'
+    return resp
